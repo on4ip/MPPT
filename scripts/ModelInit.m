a9e458f8a705efafc8207a42c15639cc50d3857a
Андрей Владимирович Ермаков <on4ip84@gmail.com>
2021-10-19 21:18:47 +0300

    В модель добавлены автотрансформаторы в качестве выходных индуктивностей

--- scripts/ModelInit.m
+++ scripts/ModelInit.m
@@ -1,2 +1,12 @@
 %%Load data structures
 load('Busses.mat');
+%%PWM base frequency
+F_PWM=50000;
+%%Regulators 
+Kp_V=0.8;
+Ki_V=100/F_PWM;
+
+%%%%%%%%%%%%%%%%%%%%%%
+BW=60000;
+Kp_I=200e-6*BW;
+Ki_I=150e-3*BW/F_PWM;
